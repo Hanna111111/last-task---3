5f5780bee106f15ea0fd08a1792af8cea513ede9
HannaAgata <77154229+Hanna111111@users.noreply.github.com>
2022-01-17 22:59:48 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct TDays
+{
+  int days;
+  int task;
+
+};
+
+void showTasks(TDays car)
+{
+car.name = "costam";
+cout<<"car : "<<&car<<" " <<car.maxspeed <<" "<<car.name << " " <<car.prize<<endl ;  
+    
+}
+
+int main()
+{
+    
+
+    return 0;
+}
