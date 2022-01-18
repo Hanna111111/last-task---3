80a50d37042143d93fa5ecc7b9f889c089e91fd7
HannaAgata <77154229+Hanna111111@users.noreply.github.com>
2022-01-18 17:01:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-struct TDays
+struct student
 {
-  int days;
-  int task;
-
-};
-
-void showTasks(TDays car)
-{
-car.name = "costam";
-cout<<"car : "<<&car<<" " <<car.maxspeed <<" "<<car.name << " " <<car.prize<<endl ;  
+    char nameOfTheTask[50];
+    int data;
+    float Priority;
     
-}
+} s[10];
 
 int main()
 {
-    
+    cout << "Enter description of: " << endl;
 
-    return 0;
+    // storing information
+    for(int i = 0; i < 10; ++i)
+    {
+        s[i].data = i+1;
+        cout << "Your data: " << s[i].data << "," << endl;
+
+        cout << "Enter name of the Task: ";
+        cin >> s[i].nameOfTheTask;
+
+        cout << "Enter priority: ";
+        cin >> s[i].Priority;
+
+        cout << endl;
+    }
+
+    cout << "Displaying Information: " << endl;
+
+    // Displaying information
+    for(int i = 0; i < 10; ++i)
+    {
+        cout << "\nYour Data: " << i+1 << endl;
+        cout << "Name of the Task: " << s[i].nameOfTheTask << endl;
+        cout << "Priority: " << s[i].Priority << endl;
+    }
 }
