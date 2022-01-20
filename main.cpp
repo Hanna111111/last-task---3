92023692ab37294187f213e9832ff08ff4a66c09
HannaAgata <77154229+Hanna111111@users.noreply.github.com>
2022-01-20 07:26:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,65 @@
 #include <iostream>
+
 using namespace std;
 
-struct student
-{
-    char nameOfTheTask[50];
-    int data;
-    float Priority;
-    
-} s[10];
+struct ToDo {
+    string Data;
+    string NameOfTheTask;
+    int Priority;
+};
 
 int main()
 {
-    cout << "Enter description of: " << endl;
-
-    // storing information
-    for(int i = 0; i < 10; ++i)
-    {
-        s[i].data = i+1;
-        cout << "Your task: " << s[i].data << "," << endl;
-
-        cout << "Enter name of the Task: ";
-        cin >> s[i].nameOfTheTask;
-
-        cout << "Enter priority: ";
-        cin >> s[i].Priority;
-
-        cout << endl;
+    
+    ToDo arrMyTask[3];
+    
+    //input
+    for (int i = 0; i < 3; i++) {
+    cout << "Please, enter Data: " << endl;
+    cin >> arrMyTask[i].Data;
+    
+    cout << "Please, enter the name of the task: " << endl;
+    cin >> arrMyTask[i].NameOfTheTask;
+    
+    cout << "Please, enter the Priority: " << endl;
+    cin >> arrMyTask[i].Priority;
     }
+    
+    //cout<< first.Data << endl;
+    //cout<< arrMyTask[0].Data << endl;
+    
+    //мы можем записать как first == arrMyTask[0] 
+    
+    cout<<"===================\n";
+    cout<<"===================\n";
+    
+    //output    
+      for(int i = 0; i < 3; i++){
+          cout << arrMyTask[i].Data << endl;
+          cout << arrMyTask[i].NameOfTheTask << endl;
+          cout << arrMyTask[i].Priority << endl;
+      }
+    
+    //   //cout <<  first.Data <<endl;
+    //     cout << arr[0].Data     <<endl;
+    //     cout << arr[0].NameOfTheTask << endl;
+    //     cout << arr[0].Priority << endl;
+        
+    //     cout<<"===================\n";
+        
+    //     cout << arr[1].Data << endl;
+    //     cout << arr[1].NameOfTheTask << endl;
+    //     cout << arr[1].Priority << endl;
+        
+    //     cout<<"===================\n";
+        
+    //     cout << arr[2].Data << endl;
+    //     cout << arr[2].NameOfTheTask << endl;
+    //     cout << arr[2].Priority << endl;
+        
+        
+    
+ 
 
-    cout << "Displaying Information: " << endl;
-
-    // Displaying information
-    for(int i = 0; i < 10; ++i)
-    {
-        cout << "\nYour Data: " << i+1 << endl;
-        cout << "Name of the Task: " << s[i].nameOfTheTask << endl;
-        cout << "Priority: " << s[i].Priority << endl;
-    }
+    return 0;
 }
