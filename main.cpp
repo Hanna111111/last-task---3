ce3bf98b6c28a37d4cb52da3a27d8c23270dd268
HannaAgata <77154229+Hanna111111@users.noreply.github.com>
2022-01-22 13:50:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,58 +8,105 @@ struct ToDo {
     int Priority;
 };
 
-int main()
-{
+
+void InputElements(ToDo *&arrMyTask,int &size){
+    ToDo yourTask;
     
-    ToDo arrMyTask[3];
-    
-    //input
-    for (int i = 0; i < 3; i++) {
     cout << "Please, enter Data: " << endl;
-    cin >> arrMyTask[i].Data;
+    cin >> yourTask.Data;
     
+    cin.ignore();
     cout << "Please, enter the name of the task: " << endl;
-    cin >> arrMyTask[i].NameOfTheTask;
+    getline(cin, yourTask.NameOfTheTask);
     
     cout << "Please, enter the Priority: " << endl;
-    cin >> arrMyTask[i].Priority;
+    cin >> yourTask.Priority;
+    
+    arrMyTask[size] = yourTask;
+    size++;
+
+}
+
+
+void AddElement(ToDo *&arrMyTask, int &size, const ToDo value){
+    ToDo *newArr = new ToDo[size+1];
+    
+    
+    for (int i = 0; i < size; i++) {
+        newArr[i] = arrMyTask[i];
     }
     
-    //cout<< first.Data << endl;
-    //cout<< arrMyTask[0].Data << endl;
+    InputElements(newArr, size);
     
-    //мы можем записать как first == arrMyTask[0] 
+    delete [] arrMyTask;
+    arrMyTask = newArr;
     
-    cout<<"===================\n";
-    cout<<"===================\n";
-    
-    //output    
-      for(int i = 0; i < 3; i++){
+}
+
+
+void OuputElements(ToDo *arrMyTask,int size){
+     
+      for(int i = 0; i < size; i++){
+          cout << "Index of your task: "<<i<<endl;
           cout << arrMyTask[i].Data << endl;
           cout << arrMyTask[i].NameOfTheTask << endl;
           cout << arrMyTask[i].Priority << endl;
+          cout << "------------------- "<< endl;
       }
+}
+int main()
     
-    //   //cout <<  first.Data <<endl;
-    //     cout << arr[0].Data     <<endl;
-    //     cout << arr[0].NameOfTheTask << endl;
-    //     cout << arr[0].Priority << endl;
-        
-    //     cout<<"===================\n";
-        
-    //     cout << arr[1].Data << endl;
-    //     cout << arr[1].NameOfTheTask << endl;
-    //     cout << arr[1].Priority << endl;
-        
-    //     cout<<"===================\n";
-        
-    //     cout << arr[2].Data << endl;
-    //     cout << arr[2].NameOfTheTask << endl;
-    //     cout << arr[2].Priority << endl;
-        
-        
-    
- 
 
+//"Main menu: "
+{
+    
+    ToDo value;
+    int size = 0;
+    ToDo *arrMyTask = new ToDo[size];
+    //cout<<"Input your size1: "<<size<<endl;
+    
+    //InputElements(arrMyTask,size);
+    bool needed = true;
+    int selection;
+    //while -> switch case
+    //добавить, вывести, выйти из программы и дефолт
+    while(needed){
+        cout << "Please, select the operation: " << endl;
+        cout << "Please, enter 1 to add your task." << endl;
+        cout << "Please, enter 2 to output your information." << endl;
+        cout << "Please, enter 3 to exit your program." << endl;
+        
+        cin >> selection;
+        
+        switch(selection){
+            case 1:
+            cout << "You chose to add your task." << endl;
+            AddElement(arrMyTask, size, value);
+            break;
+            
+            case 2: 
+            cout << "\n=======================\n" <<endl;
+            cout << "Here's your information:" <<endl;
+            
+            OuputElements(arrMyTask,size);
+            break;
+            
+            case 3:
+                needed = false;
+                cout << "Exit from the program." <<endl;
+            break;
+            
+            default:
+            cout<< "I don't know this type of operation" << endl;
+            break;
+           
+
+        }
+    }
+    
+    
+    
+    
+    
     return 0;
 }
